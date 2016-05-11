ff5495474643453a1736ccf9bad78bed4371363e
shreel <shreel@promactinfo.com>
2016-05-11 16:02:53 +0530

    dev created

--- testVersioning/testVersioning/ViewController.swift
+++ testVersioning/testVersioning/ViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Comment One - dev created
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
