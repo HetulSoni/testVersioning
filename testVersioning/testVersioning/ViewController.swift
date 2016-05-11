05177d1cb01577925098d53e1a31a23299eeda53
Hetul <Hetul@jimits-mac-mini.local>
2016-05-11 15:49:46 +0530

    initial commit

---
+++ testVersioning/testVersioning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testVersioning
+//
+//  Created by Hetul on 11/05/16.
+//  Copyright © 2016 Promact. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
